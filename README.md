65a4b31bd2de63ad1765f0069d762819d3ccd8ce
hobbyquaker <bastiraff@googlemail.com>
2020-01-03 00:42:50 +0100

    mvp

---
+++ README.md
@@ -0,0 +1,17 @@
+# node-red-contrib-wled
+
+__[Node-RED](https://nodered.org/) Nodes to control the fabulous [WLED](https://github.com/Aircoookie/WLED)__
+
+Requires WLED >= 0.9
+
+
+## Todo
+
+* [ ] Travis job for XO linting
+* [ ] Poll and output status in configurable interval
+* [ ] Documentation
+* [ ] Segment support
+
+## License
+
+MIT (c) Sebastian Raff
---
+++ nodes/wled.js
@@ -0,0 +1,49 @@
+const discover = require('../lib/discover');
+const Wled = require('../lib/wled');
+
+module.exports = function (RED) {
+    RED.httpAdmin.get('/wled/discover', /*RED.auth.needsPermission('wled.read'),*/ (req, res) => {
+        discover().then(data => {
+            res.status(200).send(data);
+        }).catch(err => {
+            res.status(500).send(err.message);
+        });
+    });
+
+    class WledNode {
+        constructor(config) {
+            RED.nodes.createNode(this, config);
+
+            if (!config.address) {
+                this.error('wled address missing');
+                return;
+            }
+
+            this.log('wled ' + config.address);
+            this.wled = new Wled(config.address);
+
+            this.wled.on('connected', () => {
+                this.status({fill: 'green', shape: 'dot', text: 'connected'});
+            });
+
+            this.on('input', (msg, send, done) => {
+                if (typeof msg.payload === 'boolean') {
+                    this.wled.setState({on: msg.payload}).then(done).catch(err => done(err.message));
+                } else if (typeof msg.payload === 'number') {
+                    this.wled.setState({bri: msg.payload}).then(done).catch(err => done(err.message));
+                } else if (typeof msg.payload === 'object') {
+                    this.wled.setState(msg.payload).then(done).catch(err => done(err.message));
+                } else if (typeof msg.payload === 'string') {
+                    if (this.wled.effects.includes(msg.payload)) {
+                        this.wled.setState({seg: [{fx: this.wled.effects.indexOf(msg.payload)}]}).then(done).catch(err => done(err.message));
+                    } else if (this.wled.palettes.includes(msg.payload)) {
+                        this.wled.setState({seg: [{pal: this.wled.palettes.indexOf(msg.payload)}]}).then(done).catch(err => done(err.message));
+                    } else if (this.wled.match(/^ps[0-9]+$/)) {
+
+                    }
+                }
+            });
+        }
+    }
+    RED.nodes.registerType("wled", WledNode);
+};
